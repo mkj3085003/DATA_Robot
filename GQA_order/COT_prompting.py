f724abd5507463370295509a34e480848ed97028
Candyaner <yanerqx@gmail.com>
2023-11-10 20:56:07 +0800

    add object data and apikey

--- GQA_order/COT_prompting.py
+++ GQA_order/COT_prompting.py
@@ -1,3 +1,10 @@
 import openai
+from VLN_find_chair.model.encode_conversation import get_completion
 api_key = "sk-z2ztez3nzdjRerVojn5LcTSsE5JzXxuDWk3oRHvigO0RzPHV"
+
+#15:34领取可用：
+#sk-Nqaedek5GkD6fC9SVyMwVTCEVeSIh5WB9EhQjAFWqjNdOZ0R
+#sk-qoXUG1ZD6BI2Lkfov8fOir8bI1dJkTd9hgEZhrHskejp15eM
+#sk-GTM6iyTPm0ALZaUUZL5Dq2LuKTHv6Qm01s4TNBAh6ACAsUM2
+
 openai.api_base = "https://openkey.cloud/v1"
