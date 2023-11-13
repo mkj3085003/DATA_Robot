1a4a36586aa0896c8b4439cef57df19d3cbc8661
Candyaner <yanerqx@gmail.com>
2023-11-13 18:35:31 +0800

    finish the GQA

--- GQA_order/COT_prompting.py
+++ GQA_order/COT_prompting.py
@@ -15,16 +15,7 @@ API_KEY = "sk-z2ztez3nzdjRerVojn5LcTSsE5JzXxuDWk3oRHvigO0RzPHV"
 
 openai.api_key = API_KEY
 openai.api_base = "https://openkey.cloud/v1"
-# openai.api_base = "https://openkey.cloud/v1"
-prompt_start={'role':'system','content':"""
-You are a cafe waiter robot. Please follow the steps below to assist customers in ordering until the customer indicates that they no longer need the service and returns the collected order information:
-1. After the customer places an order, tell the customer the menu information
-2. Talk to the customer and ask about their ordering preferences and preferences until the customer says they no longer need the service.
-3. Collect information and process it, and output the order information in JSON format.
-The JSON format should contain the following fields: "object", "num", "prefrence"
-The menu is as follows:
-
-"""}
+#多任务，多轮对话理解与记忆
 class OrderAgent(ChatBot):
     def __init__(self,menu_path="data_object.json") -> None:
         super().__init__()
@@ -33,40 +24,95 @@ class OrderAgent(ChatBot):
             MENU = json.loads(MENU_FILE)
         self.menu=MENU['food']
         self.conversation = []
-        self.final_return = []
                             #角色+任务+注意事项/实现细节+
         self.start_prompt={'role': 'system', 'content':
                             f"""You are an ordering robot that automatically collects order information for cafe.The user is a customer.
-                            the menu from cafe includes:\
-                                {self.menu}
-
-                            there are three things you need to do:
-                            First, greet with the customerand offer him the menu.
-                            Second, ask him for the item and number he want to order.
-                            Last,return a json file of the order.
-                            
+                            the menu from caffe is:{self.menu}
+                            Follow these steps to collect and format the order:
+                            1. greet and offer him the menu.
+                            2. ask him for the item and number he want to order.options of item must from menu.
+                            3. return a json file of the order.
                             Please make sure to specify all options, quantities, so that the item is uniquely identifiable from the menu.
-                            The ordering conversation should end within 4 inquiries. 
-                            
-                            there are also three things you need pay attentions to:
+                            there are three things you need pay attentions to:
                             1. Your response should be presented in a brief, very casual and friendly style.
-                            2. If you judge that the customer has finished ordering,use the following sentence as the first sentence of your reply:
-                            'Ordering is over, I will repeat your order.' 
-                            3. The order item should be selected from the menu.
-                            4. The ordering conversation should end within 4 inquiries
-                            
+                            2. Check whether the user's anwser in the conversation contains food options in the menu and the corresponding quantity.if so,reply:'Ordering is over, I will repeat your order.' 
+                            3. if the food option not find in menu.ask user to order from the menu again.
                             """,
-                            'role': 'assistant', 'content': """It's my pleasure to serve you. What would you like to order? here is the menu \n {self.menu}"""
+                            'role': 'assistant', 'content': """It's my pleasure to serve you. What would you like to order? here is the menu"""
                             }
         self.conversation.append(self.start_prompt)
         self.end_prompt = {'role': 'system', 'content': """The conversation is over.
-                         Please process the talk and return the guest's needs in JSON format.
+                         Please process the talk and return all the guest's needs in JSON format.
                            Format:
-                           {
-                           'object': item in the menu
-                           'num':num of ordering
+                           [{
+                           'object': (options:'Cake', 'Walnut', 'Mangosteen', 'SourMilkDrink', 'Banana', 'Bernachon', 'Date', 'HamSausage', 'SesameSeedCake', 'Watermelon', 'NFCJuice', 'Dunian', 'StickyNotes', 'Garlic', 'TeaTray', 'Bread', 'SpringWater', 'Softdrink', 'Gum', 'Apple', 'CoconutMilk', 'YogurtDrink', 'Yogurt', 'MilkDrink', 'OrangeJuice', 'Coffee', 'ADMilk', 'CoconutWater', 'Orange', 'CandyCase', 'Chips', 'TennisBall', 'Milk')
+                           'num':number of the objects
                            }
+                           ]
+                           for example:
+                            user:I want a glass of milk and a OrangeJuice for my daughter.
+                            return json:[{
+                            "object" : "Milk",
+                            "num" : 1
+                            },
+                            {
+                            "object" : "OrangeJuice"
+                            "num" : 1
+                            }]
+                           Please ensure that each value in the JSON is selected from the most similar content given in the customer's response. You can find these values in the customer's reply.
                            """}
+    
+    def if_end(self):
+        input = str(self.conversation)#判断是否结束对话
+        prompt=[{'role':'user','content':f"""{input}
+                Please check whether the user's information in the above conversation contains food items and the corresponding quantity. If so, answer me "True", otherwise reply "False"."""
+                }]
+        ifend = self.get_message_completion(prompt)
+        print("if end:",ifend)
+        if ifend.lower().strip() == "true":
+            return True
+        else:
+            return False
+        
+    def if_menu(self):
+        input = str(self.conversation)#判断是否结束对话
+        prompt=[{'role':'user','content':f"""{input}
+                Please check whether the user's information in the above conversation contains food items comes from the options:'Cake', 'Walnut', 'Mangosteen', 'SourMilkDrink', 'Banana', 'Bernachon', 'Date', 'HamSausage', 'SesameSeedCake', 'Watermelon', 'NFCJuice', 'Dunian', 'StickyNotes', 'Garlic', 'TeaTray', 'Bread', 'SpringWater', 'Softdrink', 'Gum', 'Apple', 'CoconutMilk', 'YogurtDrink', 'Yogurt', 'MilkDrink', 'OrangeJuice', 'Coffee', 'ADMilk', 'CoconutWater', 'Orange', 'CandyCase', 'Chips', 'TennisBall', 'Milk'. If so, answer me "True", otherwise reply "False"."""
+                }]
+        ifmenu = self.get_message_completion(prompt)
+        print("if menu:",ifmenu )
+        if ifmenu.lower().strip() == "true":
+            return True
+        else:
+            if self.conversation :
+                self.conversation.append({'role':'system','content':f"the ordering is not on the menu.offer him the menu again and let him choose from the menu and tell the quantities,menu is {self.menu}."})
+                reply = self.get_message_completion(self.conversation, temperature=0.1)
+                self.conversation.append({'role': 'assistant', 'content': reply})
+                print('order bot:', reply)
+            return False
+
+        
+    def chat(self,temp):
+        count=0
+        while True:
+            # 用户输入
+            if( self.if_end() and self.if_menu() and count>2) or count > 5:#限制对话强制小于5轮不然说个没完没了.
+                print("===============Order corversation is over===============")
+                # 结束对话
+                self.conversation.append(self.end_prompt)
+                return self.conversation
+            else:
+                input_from_user = str(input("user:"))
+                # self.user.append({'role': 'user', 'content': input_from_user})
+                self.conversation.append({'role': 'user', 'content': input_from_user})
+
+                # 模型回复
+                reply = self.get_message_completion(self.conversation, temperature=temp)
+                self.conversation.append({'role': 'assistant', 'content': reply})
+                count += 1
+                print('order bot:', reply)
+                # 判断是否结束对话
+                
     
 
     
